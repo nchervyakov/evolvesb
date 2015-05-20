86b3bfccf91ae68aa0c05a2c11312a15e2eeb4dc
Nikolay Chervyakov <nick.chervyakov@gmail.com>
2015-05-20 18:34:07 +0600

    Add google marketing tag.

--- assets/views/common/end.php
+++ assets/views/common/end.php
@@ -1,2 +1,23 @@
+<?php /*    <!-- Код тега ремаркетинга Google -->
+    <!--------------------------------------------------
+    С помощью тега ремаркетинга запрещается собирать информацию, по которой можно идентифицировать личность пользователя. Также запрещается размещать тег на страницах с контентом деликатного характера.
+    Подробнее об этих требованиях и о настройке тега читайте на странице http://google.com/ads/remarketingsetup.
+    --------------------------------------------------->
+*/ ?>
+    <script type="text/javascript">
+        /* <![CDATA[ */
+        var google_conversion_id = 955137434;
+        var google_custom_params = window.google_tag_params;
+        var google_remarketing_only = true;
+        /* ]]> */
+    </script>
+    <script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+    </script>
+    <noscript>
+        <div style="display:inline;">
+            <img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/955137434/?value=0&amp;guid=ON&amp;script=0"/>
+        </div>
+    </noscript>
+
     </body>
 </html>
